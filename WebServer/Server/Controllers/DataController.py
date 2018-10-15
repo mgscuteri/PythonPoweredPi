870a80a0e43f6b3a3ba537c0d083241318737c0e
mike <scuteri.michael@gmail.com>
2018-10-14 22:17:44 -0400

    Added data controller/io

---
+++ WebServer/Server/Controllers/DataController.py
@@ -0,0 +1,53 @@
+import json
+import http.server
+import socketserver
+import os
+import sys
+import time
+import random
+import asyncio
+import pickle
+from aiohttp import web
+
+class DataController:
+    currentDirectory = ''
+    dataBaseDirectory = ''
+    controllerPrefix = '/data'
+    app = web.Application()
+
+    def __init__(self, app, currentDirectory):
+        self.app = app
+        self.currentDirectory = currentDirectory
+        self.dataBaseDirectory = self.currentDirectory + '/WebServer/Server/Data/dataBase.txt'
+        app.router.add_route('GET', self.controllerPrefix + '/registrations', self.getRegistrations)
+        app.router.add_route('POST', self.controllerPrefix + '/registrations', self.addRegistration)
+        app.router.add_route('GET', self.controllerPrefix + '/registrations/{id}', self.getRegistrationById)
+
+    async def addRegistration(self, request):
+        dataBase = self._getRegistrationDataBase()
+        index = str(len(dataBase)+1)
+        postBody = await request.read()
+        postBodyParsed = json.loads(postBody)
+        newRow = {"Name":postBodyParsed["Name"], "emailAddress": postBodyParsed["emailAddress"] }
+        dataBase[index] = newRow
+        dePickledDataBase = open(self.currentDirectory + '/WebServer/Server/Data/dataBase.txt', 'wb')
+        pickler = pickle.Pickler(dePickledDataBase)
+        pickler.dump(dataBase)
+        dePickledDataBase.close()
+        return web.json_response(index)
+
+    def getRegistrations(self, request):
+        dataBase = self._getRegistrationDataBase()
+        return web.json_response(dataBase)
+
+    def getRegistrationById(self, request):
+        dataBase = self._getRegistrationDataBase()
+        requestedId = (request.match_info['id'])
+        return dataBase[requestedId]
+
+    def _getRegistrationDataBase(self):
+        dataBaseFile = open(self.currentDirectory + '/WebServer/Server/Data/dataBase.txt', 'rb')
+        dataBasePickler = pickle.Unpickler(dataBaseFile)
+        dataBase = dataBasePickler.load()
+        dataBaseFile.close()
+        return dataBase
--- WebServer/Server/Server.py
+++ WebServer/Server/Server.py
@@ -1,3 +1,4 @@
+import json
 import _thread
 import http.server
 import socketserver
@@ -7,84 +8,40 @@ import time
 import random
 import asyncio
 from aiohttp import web
+#HTTP controllers
+from Controllers.StaticContentController import StaticContentController
+from Controllers.NumbersController import NumbersController
+from Controllers.DataController import DataController
 
-#Globals
-WebServerRunning = False
-RestServerRunning = False
-Run = True
-
-tempDb = [
-    {
-        'id': 1,
-        'name': u'Buy groceries',
-    },
-    {
-        'id': 1,
-        'name': u'Buy groceries',
-    },
-]
-
-#Serves up REST endpoints
-def LaunchRestServer():
+def LaunchServer():
+    print("Launching REST Server")
+    currentDirectory = os.getcwd()
+    app = web.Application()
+    #Instantiate HTTP Controllers 
+    app_server = StaticContentController(app, currentDirectory)
+    app = app_server.app
+    app_server = NumbersController(app, currentDirectory)
+    app = app_server.app
+    app_server = DataController(app, currentDirectory)
+    app = app_server.app
+    #Launch Server
+    loop = asyncio.get_event_loop()
+    handler = app.make_handler()
+    f = loop.create_server(handler, '0.0.0.0', 8080)
+    srv = loop.run_until_complete(f)
+    print('serving on', srv.sockets[0].getsockname())
     try:
-        print("Launching REST Server")
-        
-        currentDirectory = os.getcwd()
-        reactBuildLocation = currentDirectory + '/WebServer/Web/scripts/site/build/'
-        #Static Content Functions
-        def getHomePage(request):
-            return web.FileResponse(reactBuildLocation +'index.html')
-        def getJsServiceWorker(request):
-            return web.FileResponse(reactBuildLocation +'service-worker.js')
-        def getJs(request):
-            jsFilePath = reactBuildLocation + '/static/js/{}'.format(request.match_info['jsFileName'])
-            return web.FileResponse(jsFilePath)
-        def getCss(request):
-            cssFilePath = reactBuildLocation + '/static/css/{}'.format(request.match_info['cssFileName'])
-            return web.FileResponse(cssFilePath)
-        def getMedia(request):
-            mediaFilePath = reactBuildLocation + '/static/media/{}'.format(request.match_info['mediaFileName'])
-            return web.FileResponse(mediaFilePath)
+        loop.run_forever()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        srv.close()
+        loop.run_until_complete(srv.wait_closed())
+        loop.run_until_complete(app.finish())
+    loop.close()
 
-        #Rest Content Functions
-        def getRandomNumber(request):
-            return web.json_response(random.randint(1,100))
-
-        app = web.Application()
-        #Static Content Routing
-        app.router.add_route('GET', '/', getHomePage)
-        app.router.add_route('GET', '/service-worker.js', getJsServiceWorker)
-        app.router.add_route('*', '/static/css/{cssFileName}', getCss)
-        app.router.add_route('*', '/static/js/{jsFileName}', getJs)
-        app.router.add_route('*', '/static/media/{mediaFileName}', getRandomNumber)
-
-        #Rest Content
-        app.router.add_route('GET', '/rand', getRandomNumber)
-
-        print('5')
-        loop = asyncio.get_event_loop()
-        print('6')
-        handler = app.make_handler()
-        f = loop.create_server(handler, '0.0.0.0', 8080)
-        srv = loop.run_until_complete(f)
-        print('serving on', srv.sockets[0].getsockname())
-        try:
-            loop.run_forever()
-        except KeyboardInterrupt:
-            pass
-        finally:
-            srv.close()
-            loop.run_until_complete(srv.wait_closed())
-            loop.run_until_complete(app.finish())
-        loop.close()
-
-        return True
-    except:
-        return False
-
-#Main
 if __name__ == '__main__':
-    RestServerRunning = LaunchRestServer()
+    LaunchServer()
     
 
 
