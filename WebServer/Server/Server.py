ed398247a99f43af6582e0bf674b7f081734bc84
mgscuteri <scuteri.michael@gmail.com>
2019-02-23 20:11:10 -0500

    changed webserver port

--- WebServer/Server/Server.py
+++ WebServer/Server/Server.py
@@ -31,7 +31,7 @@ def LaunchServer():
     #Launch Server
     loop = asyncio.get_event_loop()
     handler = app.make_handler()
-    f = loop.create_server(handler, '0.0.0.0', 8080)
+    f = loop.create_server(handler, '0.0.0.0', 8081)
     srv = loop.run_until_complete(f)
     print('serving on', srv.sockets[0].getsockname())
     try:
