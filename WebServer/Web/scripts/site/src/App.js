49ff7b9165bc96603f7f3aa0fc6a98ecdb421ad0
mgscuteri <scuteri.michael@gmail.com>
2019-02-24 20:37:33 -0500

    Updated copy on homepage

--- WebServer/Web/scripts/site/src/App.js
+++ WebServer/Web/scripts/site/src/App.js
@@ -20,7 +20,8 @@ class App extends Component {
         </div>
         <div className="appBody">
           <p className="App-intro">
-            The year is 2019.  The world has all but given up on owning your own hardware.  “Hosting a website on your own hardware is too expensive!” They say.  In stark contrast to that sentiment, I humbly present the PythonPoweredPi, a webapp that utilizes some of the most modern web technologies, and is being 100% hosted on $35 worth of hardware.   
+            The year is 2019.  The world has all but given up on owning your own hardware.  “Hosting a website on your own hardware is too expensive!” they say.  In stark contrast to that sentiment, I humbly present the PythonPoweredPi, a webapp that utilizes some of the most modern web technologies, and is being 100% hosted on $35 worth of hardware.   
+            <br/>
             <br/>
             The website is still a work in progress.  Below, is an arbitrary table that will write records to disc.  Below that, is a summary of the technologies being used.
             <br/>
