e13b8e8338120225a02454ec64292701a4bde140
mike <scuteri.michael@gmail.com>
2019-02-26 10:37:24 -0500

    Adding logging to CI/CD

--- WebServer/CI/KillServer.sh
+++ WebServer/CI/KillServer.sh
@@ -1,8 +1,8 @@
 pid=$(cat /home/pi/PythonPoweredPi/WebServer/CI/PythonPoweredPi.pid)
-echo $pid
+echo PreviousPid: $pid
 if [ "$(ps -o comm= -p "$pid")" = "python3" ]; then 
 	kill -9 "$pid"
-	echo "Process Killed"
+	echo "Process Successfully Killed"
 else 
-	echo "Process is either already dead, or running on unexpected pid!"
+	echo "Process is either already dead, or not running on pid: $pid"
 fi
