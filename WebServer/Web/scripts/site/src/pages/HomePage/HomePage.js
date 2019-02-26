29bd756cab9fea03357cacab0ad8569d9663cbed
mike <scuteri.michael@gmail.com>
2019-02-26 12:02:12 -0500

    Added title to HomePage

--- WebServer/Web/scripts/site/src/pages/HomePage/HomePage.js
+++ WebServer/Web/scripts/site/src/pages/HomePage/HomePage.js
@@ -7,13 +7,18 @@ class HomePage extends React.Component {
 
     render() {
       return (
-        <p className="App-intro">
-The year is 2019.  The world has all but given up on owning your own hardware.  “Hosting a website on your own hardware is too expensive!” they say.  In stark contrast to that sentiment, I humbly present the PythonPoweredPi, a webapp that utilizes some of the most modern web technologies, and is being 100% hosted on $35 worth of hardware.   
-<br/>
-<br/>
-The website is still a work in progress.  Below, is an arbitrary table that will write records to disc.  Below that, is a summary of the technologies being used.
-<br/>
-</p>
+        <div>
+          <h2>
+            Welcome to the PythonPoweredPi
+          </h2>
+          <p className="App-intro">
+          The year is 2019.  The world has all but given up on owning your own hardware.  “Hosting a website on your own hardware is too expensive!” they say.  In stark contrast to that sentiment, I humbly present the PythonPoweredPi, a webapp that utilizes some of the most modern web technologies, and is being 100% hosted on $35 worth of hardware.   
+          <br/>
+          <br/>
+          The website is still a work in progress.  Below, is an arbitrary table that will write records to disc.  Below that, is a summary of the technologies being used.
+          <br/>
+          </p>
+        </div>
       );
     }
   }
