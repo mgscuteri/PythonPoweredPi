6c0a8305c6c29d0dcdb54683e068f03612a4825b
mgscuteri <scuteri.michael@gmail.com>
2019-02-26 11:28:59 -0500

    whoops

--- WebServer/CI/CleanAndGetLatest.sh
+++
@@ -1,6 +0,0 @@
-cd /home/pi/PythonPoweredPi
-git config --global user.email "pythonpoweredpi@gmail.com"
-git config --global user.name "pythonpoweredpi"
-git stash save --keep-index
-git stash drop
-git pull "http://pythonpoweredpi:ThePythonCodesTonight@github.com/mgscuteri/PythonPoweredPi.git" master
