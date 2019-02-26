5b7ddde063af9288d2d7fe4d2ffdf17888b103ac
mike <scuteri.michael@gmail.com>
2019-02-26 10:59:19 -0500

    Modified CI/CD Process kill script

--- WebServer/CI/KillServer.sh
+++ WebServer/CI/KillServer.sh
@@ -1,8 +1,3 @@
 pid=$(cat /home/pi/PythonPoweredPi/WebServer/CI/PythonPoweredPi.pid)
 echo PreviousPid: $pid
-if [ "$(ps -o comm= -p "$pid")" = "python3" ]; then 
-	kill -9 "$pid"
-	echo "Process Successfully Killed"
-else 
-	echo "Process is either already dead, or not running on pid: $pid"
-fi
+kill -9 "$pid"
