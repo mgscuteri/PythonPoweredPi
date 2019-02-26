2b434374135d8f7c135533ba55ee6a5287a95771
mike <scuteri.michael@gmail.com>
2019-02-26 12:20:31 -0500

    Modified CI/CD Process Kill script

--- WebServer/CI/KillServer.sh
+++ WebServer/CI/KillServer.sh
@@ -1,3 +1,16 @@
 pid=$(cat /home/pi/PythonPoweredPi/WebServer/CI/PythonPoweredPi.pid)
 echo PreviousPid: $pid
-kill -9 "$pid"
+echo "Attempting to kill previous instance"
+
+if [ "$(ps -o comm= -p "$pid")" = "python3" ]; then 
+	echo "Previous instance found"
+	kill -9 "$pid"
+	echo "Waiting for process to die"
+	sleep 10s
+	echo "Previous instance succesfully shut down."
+else 
+	echo "failed to identify a running instance"
+fi
+
+
+
--- WebServer/CI/LaunchServer.sh
+++ WebServer/CI/LaunchServer.sh
@@ -1,6 +1,4 @@
 cd /home/pi/PythonPoweredPi/WebServer/Server
-echo "Waiting for process to die.."
-sleep 10s
 echo launching server
 python3 Server.py & 
 echo "$!" > /home/pi/PythonPoweredPi/WebServer/CI/PythonPoweredPi.pid
