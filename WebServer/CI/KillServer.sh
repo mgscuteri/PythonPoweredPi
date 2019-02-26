ba4ef19963441f3e64700053e87355403ff99dd9
mike <scuteri.michael@gmail.com>
2019-02-26 12:40:50 -0500

    Added deployment vars to .gitignore

--- WebServer/CI/KillServer.sh
+++ WebServer/CI/KillServer.sh
@@ -1,4 +1,4 @@
-pid=$(cat /home/pi/PythonPoweredPi/WebServer/CI/PythonPoweredPi.pid)
+pid=$(cat /home/pi/PythonPoweredPi/WebServer/CI/DeploymentVars/PythonPoweredPi.pid)
 echo PreviousPid: $pid
 echo "Attempting to kill previous instance"
 
@@ -10,6 +10,7 @@ if [ "$(ps -o comm= -p "$pid")" = "python3" ]; then
 	echo "Previous instance succesfully shut down."
 else 
 	echo "failed to identify a running instance"
+	raise error "Previous pid does not appear to be linked to a currently running python instance"
 fi
 
 
--- WebServer/CI/LaunchServer.sh
+++ WebServer/CI/LaunchServer.sh
@@ -1,5 +1,5 @@
 cd /home/pi/PythonPoweredPi/WebServer/Server
 echo launching server
 python3 Server.py & 
-echo "$!" > /home/pi/PythonPoweredPi/WebServer/CI/PythonPoweredPi.pid
+echo "$!" > /home/pi/PythonPoweredPi/WebServer/CI/DeploymentVars/PythonPoweredPi.pid
 
