bee41ffccfb945fa7b5b54cf46169ac241152c4c
unknown <scuteri.michael@gmail.com>
2019-02-26 16:10:25 -0500

    CSS Fixes

--- WebServer/Web/scripts/site/src/pages/HomePage/HomePage.js
+++ WebServer/Web/scripts/site/src/pages/HomePage/HomePage.js
@@ -11,13 +11,14 @@ class HomePage extends React.Component {
           <h2>
             Welcome to the PythonPoweredPi
           </h2>
-          <p className="App-intro">
-          The year is 2019.  The world has all but given up on owning your own hardware.  “Hosting a website on your own hardware is too expensive!” they say.  In stark contrast to that sentiment, I humbly present the PythonPoweredPi, a webapp that utilizes some of the most modern web technologies, and is being 100% hosted on $35 worth of hardware.   
-          <br/>
-          <br/>
-          The website is still a work in progress.  Below, is an arbitrary table that will write records to disc.  Below that, is a summary of the technologies being used.
-          <br/>
+          <p>
+            The year is 2019.  The world has all but given up on owning your own hardware.  “Hosting a website on your own hardware is too expensive!” they say.  In stark contrast to that sentiment, I humbly present the PythonPoweredPi, a webapp that utilizes some of the most modern web technologies, and is being 100% hosted on $35 worth of hardware.   
+            
           </p>
+          <p>
+            The website is still a work in progress.  Below, is an arbitrary table that will write records to disc.  Below that, is a summary of the technologies being used.
+          </p>
+          <br/>
         </div>
       );
     }
