b1521e547e997083e4b9294fa14971bf62217265
mike <scuteri.michael@gmail.com>
2019-03-03 17:21:05 -0500

    Added react routing

--- WebServer/Web/scripts/site/src/App.js
+++ WebServer/Web/scripts/site/src/App.js
@@ -1,24 +1,19 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom'
 import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
-import ThemeSongs from './pages/ThemeSongs/ThemeSongs.js'
-import HomePage from './pages/HomePage/HomePage.js'
 import './App.css';
-import TechOverview from './pages/TechOverview/TechOverview.js';
+
 import Navbar from './modules/site/navbar.js'
+import Footer from './modules/site/footer.js'
 
 
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div>
         <Navbar/>
-        <div className="appBody">
-          <HomePage/>
-          <ThemeSongs/>
-          <TechOverview/>
-        </div>
+        <Footer/>
       </div>
     );
   }
--- WebServer/Web/scripts/site/src/modules/site/navbar.js
+++ WebServer/Web/scripts/site/src/modules/site/navbar.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 import piLogo from '../../images/piLogoSmall.png';
 import ReactDOM from 'react-dom'
 import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
+import { Switch } from 'react-router-dom';
+
+import ThemeSongs from '../../pages/ThemeSongs/ThemeSongs.js'
+import HomePage from '../../pages/HomePage/HomePage.js'
+import TechOverview from '../../pages/TechOverview/TechOverview.js';
+
 
 class Navbar extends React.Component {  
     constructor(props) {
@@ -10,22 +16,30 @@ class Navbar extends React.Component {
 
     render() {
       return (
-        <div class="navBarContainer">
-          
-              <div className="logo">
+          <Router>
+            <div>
+              <div class="navBarContainer">
+                <div className="logo">
                   <a class="active" href="#home"><img src={piLogo} alt="logo"/></a>
-              </div>
-              <div class="navbar">
-                <Router>
+                </div>
+                <div class="navbar">
                   <ul class="topnav">
-                      <li><Link to="/">Home</Link></li>
-                      <li><Link to="/ThemeSongs">ThemeSongs</Link></li>
-                      <li class="right"><Link to="/About">About</Link></li>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/a/ThemeSongs">ThemeSongs</Link></li>
+                    <li class="right"><Link to="/a/About">About</Link></li>
                   </ul>
-                </Router>
+                </div>
               </div>
-            
-        </div>
+              <div className="appBody">
+                <Switch>
+                  <Route exact path="/" component={HomePage} />
+                  <Route path="/a/ThemeSongs" component={ThemeSongs} />
+                  <Route path="/a/About" component={TechOverview} />
+                  <Route render={ () => <h1>404 Error</h1> } />
+                </Switch>
+              </div>
+            </div>
+          </Router>
       );
     }
   }
