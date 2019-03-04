0d325cc8afdb53f332a6739b94066d2b66207729
mike <scuteri.michael@gmail.com>
2019-03-03 22:26:28 -0500

    CSS changes

--- WebServer/Web/scripts/site/src/modules/site/navbar.js
+++ WebServer/Web/scripts/site/src/modules/site/navbar.js
@@ -7,6 +7,7 @@ import { Switch } from 'react-router-dom';
 import ThemeSongs from '../../pages/ThemeSongs/ThemeSongs.js'
 import HomePage from '../../pages/HomePage/HomePage.js'
 import TechOverview from '../../pages/TechOverview/TechOverview.js';
+import LogIn from '../../pages/LogIn/LogIn.js';
 
 
 class Navbar extends React.Component {  
@@ -19,20 +20,20 @@ class Navbar extends React.Component {
           <Router>
             <div>
               <div class="navBarContainer">
-                <div className="logo">
-                  <a class="active" href="#home"><img src={piLogo} alt="logo"/></a>
-                </div>
                 <div class="navbar">
                   <ul class="topnav">
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/ThemeSongs">ThemeSongs</Link></li>
-                    <li class="right"><Link to="/About">About</Link></li>
+                    <li><Link to="/"><img src={piLogo} alt="logo"/></Link></li>
+                    <li class="topnav"><Link to="/">Home</Link></li>
+                    <li class="topnav, right"><Link to="/LogIn">Log In</Link></li>
+                    <li class="topnav"><Link to="/ThemeSongs">ThemeSongs</Link></li>
+                    <li class="topnav"><Link to="/About">About</Link></li>
                   </ul>
                 </div>
               </div>
               <div className="appBody">
                 <Switch>
                   <Route exact path="/" component={HomePage} />
+                  <Route path="/LogIn" component={LogIn} />
                   <Route path="/ThemeSongs" component={ThemeSongs} />
                   <Route path="/About" component={TechOverview} />
                   <Route render={ () => <h1>404 Error</h1> } />
